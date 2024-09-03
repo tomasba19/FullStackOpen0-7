e7c04867491a0d3989a418aa66aa098c8182f644
Tomas Barolo <tomasba19@gmail.com>
2024-09-02 21:37:38 -0300

    partA

--- pruebas/pruebasTeorico3/index.js
+++ pruebas/pruebasTeorico3/index.js
@@ -1,9 +1,5 @@
-const http = require("http");
 const express = require("express");
-
 const app = express();
-
-app.use(express.json());
 
 let notes = [
   {
@@ -22,6 +18,21 @@ let notes = [
     important: true,
   },
 ];
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -73,6 +84,8 @@ app.post("/api/notes", (request, response) => {
 
   response.json(note);
 });
+
+app.use(unknownEndpoint)
 
 const PORT = 3001;
 app.listen(PORT, () => {
