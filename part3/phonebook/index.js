02db742ef9bd1f4e8d72054d1dcec127d54c9350
Tomas Barolo <tomasba19@gmail.com>
2024-09-03 23:35:05 -0300

    3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 morgan.token('post-data', (req) => { 
   if (req.method === 'POST') {
@@ -93,7 +95,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
