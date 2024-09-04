cc9cbbc7f24b0121fcc06f844339c4e58c41bd04
Tomas Barolo <tomasba19@gmail.com>
2024-09-03 21:51:45 -0300

    partB

--- pruebas/pruebasTeorico3/index.js
+++ pruebas/pruebasTeorico3/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
+
+
 
 let notes = [
   {
@@ -29,6 +32,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -87,7 +91,7 @@ app.post("/api/notes", (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
